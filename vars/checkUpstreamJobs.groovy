64ca924b4602aec777e4045eaaee8545b851ac7d
h03149 <162279546+h03149@users.noreply.github.com>
2024-05-06 17:04:52 +0900

    Update checkUpstreamJobs.groovy

--- vars/checkUpstreamJobs.groovy
+++ vars/checkUpstreamJobs.groovy
@@ -1,13 +1,5 @@
-def call(upstreamJobs) {
-    def allUpstreamSuccess = upstreamJobs.every { jobName ->
-        def job = Jenkins.instance.getItemByFullName(jobName)
-        if (job) {
-            def lastBuild = job.getLastBuild()
-            return lastBuild && lastBuild.result == 'SUCCESS'
-        } else {
-            echo "Job '$jobName'을(를) 찾을 수 없습니다."
-            return false
-        }
-    }
-    return allUpstreamSuccess
+def call(stageName) {
+  def currentBuild = currentBuild()
+  def stageResult = currentBuild.rawBuild.getStage(stageName).result
+  return stageResult == 'SUCCESS'
 }
